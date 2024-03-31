2b56b174a4be2a960a5595ae2151dc1b58e78238
adnan4k <fayomuhe5@gmail.com>
2024-03-31 07:09:30 +0300

    here text added

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import Layout from "../layout/Layout";
+
+function Home() {
+  return (
+    <Layout>
+      <div className="mt-32">
+        <div className="flex flex-col justify-center items-center">
+          <div>
+            <h1 className="text-[#081E4A] text-center text-3xl font-bold">
+              Find Your Perfect Rental Home
+            </h1>
+          </div>
+          <div>
+            <p className="text-xl my-5">
+              A Great Platform to Connect users Directly with Tenant
+              and Discover Your Dream Home
+            </p>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default Home;
