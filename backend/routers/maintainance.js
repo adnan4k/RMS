31ba616b1d5d4895372a02475fc2bc555549ffcd
adnan4k <fayomuhe5@gmail.com>
2024-05-04 14:36:06 +0300

    verify token added

--- backend/routers/maintainance.js
+++ backend/routers/maintainance.js
@@ -1,9 +1,10 @@
 import express from "express"
 import { createMaintainance, getMaintenanceRequest } from "../controllers/MaintainanceRequestController.js"
+import verifyToken from "../utils/verifyToken.js"
 
 const maintainanceRouter = express.Router()
 
   maintainanceRouter.post('/create',createMaintainance)
-  maintainanceRouter.get('/get-maintenance-request',getMaintenanceRequest)
+  maintainanceRouter.get('/get-maintenance-request',verifyToken('owner'),getMaintenanceRequest)
 
   export default maintainanceRouter;
