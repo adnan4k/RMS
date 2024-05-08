284c2edff3ee4b20128f90ddcf08135345c17d34
adnan4k <fayomuhe5@gmail.com>
2024-05-09 00:20:30 +0300

    sidebar added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 import Home from './pages/Home';
+import Sidebar from './layout/Sidebar';
 
 
 
@@ -14,6 +15,8 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/sidebar" element={<Sidebar />} />
+
        
       </Routes>
     </BrowserRouter>    </div>
